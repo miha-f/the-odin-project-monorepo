5e6fa4221205d7082c2fa4c932371a1672404847
Miha F <miihafabcic@proton.me>
2025-03-19 08:23:35 +0100

    Add game logic based on buttons

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const signs = ["rock", "paper", "scissor"];
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const playAgainButton = document.querySelector("#playAgain");
+
+const signs = ["rock", "paper", "scissors"];
 
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 
 const getComputerChoice = () => {
     const choice = Math.floor(Math.random() * 3);
     return signs[choice];
-};
-
-const getHumanChoice = () => {
-    return prompt("Enter your sign, valid options: rock, paper, scissor");
 };
 
 const playRound = (humanChoice, computerChoice) => {
@@ -18,7 +20,7 @@ const playRound = (humanChoice, computerChoice) => {
 
     switch (humanChoice) {
         case 'rock':
-            if (computerChoice === 'scissor')
+            if (computerChoice === 'scissors')
                 humanScore++;
             else if (computerChoice === 'paper')
                 computerScore++;
@@ -26,10 +28,10 @@ const playRound = (humanChoice, computerChoice) => {
         case 'paper':
             if (computerChoice === 'rock')
                 humanScore++;
-            else if (computerChoice === 'scissor')
+            else if (computerChoice === 'scissors')
                 computerScore++;
             break;
-        case 'scissor':
+        case 'scissors':
             if (computerChoice === 'paper')
                 humanScore++;
             else if (computerChoice === 'rock')
@@ -37,3 +39,23 @@ const playRound = (humanChoice, computerChoice) => {
             break;
     }
 };
+
+rockButton.addEventListener("click", () => { playGame("rock"); });
+paperButton.addEventListener("click", () => { playGame("paper"); });
+scissorsButton.addEventListener("click", () => { playGame("scissors"); });
+
+const playGame = (humanSelection) => {
+    // NOTE(miha): We play 5 rounds
+    if (round < 5) {
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        round++;
+    } else {
+        if (humanScore > computerScore)
+            console.log("human wins");
+        else if (computerScore > humanScore)
+            console.log("computer wins");
+        else
+            console.log("its a draw");
+    }
+}
