01599ef49761a255e8f59757ed0b26da1a3eadd3
Miha F <miihafabcic@proton.me>
2025-03-19 09:16:54 +0100

    Add JS generated grid

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+// TODO(miha):
+//          - create function that craetes div
+//          - ask user input for grid size
+//          - add on hover event listener
+//          - add colors
+//          - add rainbow effect (random color)
+//          - add shadowing effect (we shadow current color by 10%)
+//          - 
+//          - 
+
+let gridSize = 10;
+
+const createGridElement = (r, c) => {
+    const index = r * 10 + c;
+    const div = document.createElement("div");
+    div.id = `gridElement${index}`;
+    div.classList.add("grid-element");
+    return div;
+};
+
+const createGrid = () => {
+    const container = document.querySelector(".container");
+
+    for (let r = 0; r < gridSize; r++) {
+        for (let c = 0; c < gridSize; c++) {
+            const element = createGridElement(r, c);
+            container.appendChild(element);
+        }
+    }
+};
+
+createGrid();
