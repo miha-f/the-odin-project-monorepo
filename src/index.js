3104147dd9b32b4cfadf36517a27eba16e6a85c6
Miha F <miihafabcic@proton.me>
2025-03-28 19:11:54 +0100

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import "./style.css";
 
-console.log("hello");
+import drawHome from "./pages/home.js";
 
-// TODO(miha): Put tab switching logic here
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
 
 const tabs = (function() {
-    const init = (firstPage) => { };
+    const init = (...tabMappings) => {
+        if (tabMappings.length === 0)
+            throw Error("please provide some tab mappings");
 
+        for (const { button, func } of tabMappings) {
+            button.addEventListener('click', (e) => {
+                clear();
+                func();
+            });
+        }
+
+        tabMappings[0].func();
+    };
+
+    const clear = () => {
+        const content = document.querySelector("#content");
+        while (content.firstChild)
+            content.removeChild(content.lastChild);
+    };
+
+    return { init, clear };
 })();
+
+const createTabMap = (button, func) => {
+    return { button, func };
+}
+
+tabs.init(createTabMap(homeButton, drawHome));
