9b5e8d5ff3717636c0ce941bf4a4cd9538a40bc7
Miha F <miihafabcic@proton.me>
2025-03-28 14:05:25 +0100

    Refactor game into game and gui
    - used to do all DOM related stuff in game. This commit splits game logic and gui into
      seperate "modules"

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// TODO(miha): When we have winner, game should disable -> all buttons
-
 const game = (function() {
     // NOTE(miha): How is the gameboard constructed.
     // 0 1 2
@@ -15,81 +13,38 @@ const game = (function() {
         for (var i = 0; i < 9; i++)
             gameboard.push(undefined);
 
-        const buttons = _getGameboardButtons();
-        for (let i = 0; i < buttons.length; i++) {
-            const button = buttons[i];
-            button.setAttribute("data-id", i);
-            button.addEventListener("click", (e) => {
-                e.target.disabled = true;
-                e.target.textContent = currentPlayer;
-                const id = e.target.getAttribute("data-id");
-                gameboard[id] = currentPlayer;
-
-                playRound();
-
-                _switchCurrentPlayer();
-            });
-        }
-
         currentPlayer = _getStartingPlayer();
-
-        const startingPlayerText = document.querySelector("#starting-player");
-        startingPlayerText.textContent = `${currentPlayer} starts`;
     };
 
     const playRound = () => {
         const winner = _checkWinner();
 
-        if (winner === "X")
-            xWins++;
-
-        if (winner === "O")
-            oWins++;
-
-        if (winner === "X" || winner === "O")
-            _victoryScreen(winner);
+        if (winner === "X" || winner === "O") {
+            _increaseWinnerScore(winner);
+            return winner;
+        }
 
         // NOTE(miha): Game is a draw.
         if (!gameboard.includes(undefined) && winner === undefined)
-            _victoryScreen("draw");
+            return "draw";
+
+        _switchCurrentPlayer();
     };
 
     const reset = () => {
         gameboard.length = 0;
-        // currentPlayer = "";
-        // TODO(miha): Do we need to remove event listener?
-        const buttons = _getGameboardButtons();
-        console.log(buttons);
-        for (const button of buttons) {
-            button.disabled = false;
-            button.textContent = "";
-        }
+
+    };
+
+    const setGameboard = (id) => {
+        gameboard[id] = currentPlayer;
     };
 
     const getXWins = () => xWins;
     const getOWins = () => oWins;
+    const getCurrentPlayer = () => currentPlayer;
 
-    const _getGameboardButtons = () => {
-        const buttons = document.querySelectorAll("button");
-        return [...buttons].filter((button) => button.id !== "play-again");
-    }
-    const _victoryScreen = (winner) => {
-        if (winner === "X") {
-            console.log("x wins?");
-            const playerX = document.querySelector("#player-x-score");
-            playerX.textContent = `X wins: ${getXWins()}`;
-        }
-        if (winner === "O") {
-            console.log("o wins?");
-            const playerO = document.querySelector("#player-o-score");
-            playerO.textContent = `O wins: ${getOWins()}`;
-        }
-
-        const victoryDiv = document.querySelector("#victory-message");
-        victoryDiv.style["visibility"] = "visible";
-        const victoryMessage = document.querySelector("#victory-message>h2");
-        victoryMessage.textContent = winner !== "draw" ? `${winner} wins!` : "Draw!";
-    };
+    const _increaseWinnerScore = (winner) => winner === "X" ? xWins++ : oWins++;
     const _switchCurrentPlayer = () => currentPlayer === "X" ? currentPlayer = "O" : currentPlayer = "X";
     const _getStartingPlayer = () => Math.random() > 0.5 ? "X" : "O";
     const _checkWinner = () => {
@@ -127,20 +82,86 @@ const game = (function() {
         return undefined;
     };
 
-    return { init, playRound, reset, getXWins, getOWins };
+    return { init, playRound, reset, getXWins, getOWins, getCurrentPlayer, setGameboard };
 })();
 
+// NOTE(miha): gui talks with DOM.
 const gui = (function() {
+    const init = () => {
+        game.init();
+        _startingPlayerText.textContent = `${game.getCurrentPlayer()} starts`;
+
+        const buttons = _getGameboardButtons();
+        for (let i = 0; i < buttons.length; i++) {
+            const button = buttons[i];
+            button.setAttribute("data-id", i);
+            button.addEventListener("click", (e) => {
+                e.target.disabled = true;
+                e.target.textContent = game.getCurrentPlayer();
+                const id = e.target.getAttribute("data-id");
+                game.setGameboard(id);
+                const winner = game.playRound();
+                switch (winner) {
+                    case "X":
+                    case "O":
+                    case "draw":
+                        _disableButtons();
+                        _victoryScreen(winner);
+                        break;
+                    default: break;
+                }
+            });
+        }
+
+    };
+
+    const _reset = () => {
+        currentPlayer = "";
+        const buttons = _getGameboardButtons();
+        for (const button of buttons) {
+            button.disabled = false;
+            button.textContent = "";
+        }
+
+        _victoryMessageDiv.style["visibility"] = "hidden";
+    };
+
+    const _startingPlayerText = document.querySelector("#starting-player");
+    const _xPlayerScoreText = document.querySelector("#player-x-score");
+    const _oPlayerScoreText = document.querySelector("#player-o-score");
+    const _victoryMessageDiv = document.querySelector("#victory-message");
+    const _victoryMessageText = document.querySelector("#victory-message>h2");
+    const _playAgainButton = document.querySelector("#play-again");
+
+    _playAgainButton.addEventListener('click', (e) => {
+        _reset();
+        game.reset();
+        game.init();
+    });
+
+    const _getGameboardButtons = () => {
+        const buttons = document.querySelectorAll("button");
+        return [...buttons].filter((button) => button.id !== "play-again");
+    }
+
+    const _disableButtons = () => {
+        const buttons = _getGameboardButtons();
+        for (const button of buttons) {
+            button.disabled = true;
+        }
+    }
+
+    const _victoryScreen = (winner) => {
+        if (winner === "X")
+            _xPlayerScoreText.textContent = `X wins: ${game.getXWins()}`;
+        if (winner === "O")
+            _oPlayerScoreText.textContent = `O wins: ${game.getOWins()}`;
+
+        _victoryMessageDiv.style["visibility"] = "visible";
+        _victoryMessageText.textContent = winner !== "draw" ? `${winner} wins!` : "Draw!";
+    };
+
+    return { init };
 })();
 
-const playAgainButton = document.querySelector("#play-again");
-playAgainButton.addEventListener('click', (e) => {
-    game.reset();
-    game.init();
-});
-
-game.init();
-
-// TODO(miha): Currently code is "coupled" we do everything in "module game".
-// Game should only be concered about game and we should put GUI on top of it.
-// If we can't interact from GUI with game we should add method to be able to do so.
+gui.init();
