c6233683b183974fa9bb8fe0088dd2c161832f9e
Miha F <miihafabcic@proton.me>
2025-04-02 09:18:36 +0200

    Add append, toString

--- linked_list.js
+++ linked_list.js
@@ -1,20 +1,54 @@
 const LinkedList = () => {
-    const append = (value) => { };
+    let _head = null;
+    let _size = 0;
+
+    const append = (value) => {
+        const node = Node(value);
+        if (_head === null) {
+            _head = node
+        }
+        else {
+            let curr = _head;
+            while (curr.next)
+                curr = curr.next;
+            curr.next = node;
+        }
+    };
+
     const prepend = (value) => { };
-    const size = () => { };
-    const head = () => { };
+
+    const size = () => { return _size; };
+    const head = () => { return _head; };
     const tail = () => { };
     const at = (index) => { };
     const pop = () => { };
     const contains = (value) => { };
     const find = (value) => { };
-    const toString = () => { };
+
+    const toString = () => {
+        let curr = _head;
+        let result = "";
+        while (curr) {
+            result += `( ${curr.value} ) -> `;
+            curr = curr.next;
+        }
+        result += `( ${null} )`;
+        return result;
+    };
+
     const insertAt = (value, index) => { };
     const removeAt = (index) => { };
+
+    return {
+        append, prepend, size, head, tail, at, pop, contains, find,
+        toString, insertAt, removeAt
+    };
 }
 
-const Node = (value = null, next = null) => {
-    const value = value;
-    const next = next;
+const Node = (valueParam = null, nextParam = null) => {
+    const value = valueParam;
+    const next = nextParam;
     return { value, next };
 }
+
+export { LinkedList };
