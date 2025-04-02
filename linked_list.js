b7da8a75e1105332ea204026653a59978ac30321
Miha F <miihafabcic@proton.me>
2025-04-02 08:46:00 +0200

    Add linked list method definitions

---
+++ linked_list.js
@@ -0,0 +1,20 @@
+const LinkedList = () => {
+    const append = (value) => { };
+    const prepend = (value) => { };
+    const size = () => { };
+    const head = () => { };
+    const tail = () => { };
+    const at = (index) => { };
+    const pop = () => { };
+    const contains = (value) => { };
+    const find = (value) => { };
+    const toString = () => { };
+    const insertAt = (value, index) => { };
+    const removeAt = (index) => { };
+}
+
+const Node = (value = null, next = null) => {
+    const value = value;
+    const next = next;
+    return { value, next };
+}
