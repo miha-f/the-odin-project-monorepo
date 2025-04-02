5fadc808f8e10de09a81d18ed1b2c74c2fb57e5d
Miha F <miihafabcic@proton.me>
2025-04-02 09:59:50 +0200

    Add hash

--- hash_map.js
+++ hash_map.js
@@ -8,7 +8,17 @@ const HashMap = (capacity = 16, loadFactor = 0.75) => {
         }
     }
 
-    const hash = (key) => { };
+    const hash = (key) => {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode %= _buckets.length;
+        }
+
+        return hashCode;
+    };
 
     const set = (key, value) => { };
 
