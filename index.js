f2a7ba0c4b46fb92e86a1c5b8c1c184bdb58a179
Miha F <miihafabcic@proton.me>
2025-04-03 09:20:48 +0200

    Init

---
+++ index.js
@@ -0,0 +1,4 @@
+import { knightMoves } from "./knight";
+
+
+// knightMoves();
---
+++ knight.js
@@ -0,0 +1,14 @@
+const knightMoves = (from, to) => {
+    const GRID_SIZE = 8;
+
+    const dirs = [
+        [-1, 2], [-1, -2], [-2, 1], [-2, -1],
+        [1, 2], [1, -2], [2, 1], [2, -1]
+    ];
+
+    const dfs = () => {
+    };
+
+};
+
+export { knightMoves };
