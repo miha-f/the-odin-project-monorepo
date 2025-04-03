3766d537fa2ae08aac91c9423995acfc0eba0cb4
Miha F <miihafabcic@proton.me>
2025-04-03 10:28:06 +0200

    Add algorithm, memoization

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { knightMoves } from "./knight.js";
 
 
 console.log(knightMoves([0, 0], [3, 3]));
+console.log(knightMoves([3, 3], [4, 3]));
--- knight.js
+++ knight.js
@@ -14,35 +14,47 @@ const knightMoves = (from, to) => {
     }
 
     let visited = new Set();
+    // NOTE(miha): Need to convert point to string, otherwise we save position
+    // reference and not actual position.
+    const addVisited = (x, y) => visited.add(`${x},${y}`);
+    const hasVisited = (x, y) => visited.has(`${x},${y}`);
+    const removeVisited = (x, y) => visited.delete(`${x},${y}`);
+
+    let cache = new Map();
 
     const dfs = (pos, moves = 0, path = []) => {
+        if (pos[0] === to[0] && pos[1] === to[1]) {
+            if (moves < minMoves) {
+                minMoves = moves;
+                minPath = path.slice();
+            }
+            return;
+        }
+
+        const key = `${pos[0]},${pos[1]}`;
+        if (cache.has(key) && cache.get(key) <= moves) {
+            return;
+        }
+        cache.set(key, moves);
+
         for (const dir of dirs) {
-            let newPos = pos;
-            newPos[0] += dir[0];
-            newPos[1] += dir[1];
+            let newPos = [0, 0];
+            newPos[0] = pos[0] + dir[0];
+            newPos[1] = pos[1] + dir[1];
 
-            if (visited.has(newPos))
-                continue;
-
-            if (newPos === to) {
-                if (moves < minMoves) {
-                    minMoves = moves;
-                    minPath = path;
-                }
+            if (inBound(newPos) && !hasVisited(newPos[0], newPos[1])) {
+                addVisited(newPos[0], newPos[1]);
+                path.push([...newPos]);
+                dfs(newPos, moves + 1, path);
+                path.pop();
+                removeVisited(newPos[0], newPos[1]);
             }
-
-            if (inBound(newPos)) {
-                visited.add(newPos);
-                dfs(newPos, moves + 1, path.push(newPos));
-            }
-
-            visited.delete(newPos);
         }
     };
 
     dfs(from);
 
-    return minPath;
+    return [from, ...minPath];
 };
 
 export { knightMoves };
