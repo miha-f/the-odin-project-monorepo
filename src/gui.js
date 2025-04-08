8e5154b925c0e7ba111149d174cd81e3289dc0e3
Miha F <miihafabcic@proton.me>
2025-04-08 14:47:53 +0200

    Add ship placement on gameboard
    - when hovering gameboard with mouse, show ship location
    
    Add click to place ships
    
    Remove ship from "shelf"
    
    - shelf is where you select ship to pick

--- src/gui.js
+++ src/gui.js
@@ -1,4 +1,5 @@
 import { SIZE } from "./gameboard.js";
+import { State } from "./state.js";
 
 // TODO(miha): This will be moved to the game factory! also remove from gameboard.js
 const _defaultGameboardRules = {
@@ -15,8 +16,17 @@ const _defaultGameboardRules = {
 const GUI = (game) => {
     const main = document.querySelector("main");
 
+
+    let state = State();
+
     const drawShips = () => {
         const div = document.querySelector(".ships");
+        let shipCount = 0;
+
+        const rotateButton = document.querySelector("button");
+        rotateButton.addEventListener("click", () => {
+            state.shipSelector.rotate = !state.shipSelector.rotate;
+        });
 
         for (const [k, v] of Object.entries(_defaultGameboardRules.shipToCountMap)) {
             const shipLength = k;
@@ -24,6 +34,7 @@ const GUI = (game) => {
                 const ship = document.createElement("div");
                 ship.classList.add("ship");
                 ship.classList.add("always-border");
+                ship.setAttribute("data-id", `ship-${shipCount}`);
                 for (let j = 0; j < shipLength; j++) {
                     const cell = document.createElement("div");
                     cell.classList.add("cell");
@@ -31,10 +42,21 @@ const GUI = (game) => {
                 }
 
                 ship.addEventListener("click", (e) => {
-                    if (!e.target.parentNode.classList.contains("ships"))
-                        e.target.parentNode.classList.toggle("ship-border");
+                    if (e.target.parentNode.classList.contains("ships"))
+                        return;
+
+                    const ships = document.querySelectorAll(".ship");
+                    ships.forEach((ship) => {
+                        ship.classList.remove("ship-border");
+                    });
+
+                    state.shipSelector.length = e.target.parentNode.querySelectorAll(".cell").length;
+
+                    state.shipSelector.id = e.target.parentNode.getAttribute("data-id");
+                    e.target.parentNode.classList.toggle("ship-border");
                 });
 
+                shipCount++;
                 div.appendChild(ship);
             }
         }
@@ -49,6 +71,66 @@ const GUI = (game) => {
                 const cell = document.createElement("div");
                 cell.classList.add("cell");
                 row.appendChild(cell);
+
+                cell.setAttribute("data-id", `cell-${r * SIZE + c}`);
+
+                cell.addEventListener("mouseenter", (e) => {
+                    const id = e.target.getAttribute("data-id");
+                    if (state.shipSelector.id) {
+                        const cells = div.querySelectorAll(".cell");
+                        cells.forEach((cell) => {
+                            cell.classList.remove("hover");
+                        });
+
+                        state.shipSelector.cells = [];
+
+                        // NOTE(miha): HORIZONTAL direction
+                        if (!state.shipSelector.rotate) {
+                            for (let i = 0; i < state.shipSelector.length; i++) {
+                                const newId = Number(id.split("-")[1]) + (i * SIZE);
+                                if (newId < 100) {
+                                    const cell = div.querySelector(`[data-id="cell-${newId}"]`);
+                                    cell.classList.add("hover");
+                                    state.shipSelector.cells.push(cell);
+                                }
+                            }
+                        } else {
+                            for (let i = 0; i < state.shipSelector.length; i++) {
+                                const oldId = Number(id.split("-")[1]);
+                                const newId = oldId + i;
+                                // NOTE(miha): Make sure that oldId (cursor loc) and newId are on the same column.
+                                if (newId < 100 && Math.floor(oldId / 10) === Math.floor(newId / 10)) {
+                                    const cell = div.querySelector(`[data-id="cell-${newId}"]`);
+                                    cell.classList.add("hover");
+                                    state.shipSelector.cells.push(cell);
+                                }
+                            }
+                        }
+
+
+                    }
+                });
+
+                cell.addEventListener("click", (e) => {
+                    if (!state.shipSelector.id)
+                        return;
+
+                    state.shipSelector.cells.forEach((cell) => {
+                        cell.classList.add("placed");
+                    });
+
+                    const ship = document.querySelector(`[data-id="${state.shipSelector.id}"]`);
+                    ship.remove();
+
+                    state.shipSelector = {
+                        id: "",
+                        rotate: false,
+                        length: -1,
+                        cells: [],
+                    };
+
+
+                });
             }
             div.appendChild(row);
         }
---
+++ src/state.js
@@ -0,0 +1,12 @@
+const State = () => {
+    let shipSelector = {
+        id: "",
+        rotate: false,
+        length: -1,
+        cells: [],
+    };
+
+    return { shipSelector };
+};
+
+export { State };
