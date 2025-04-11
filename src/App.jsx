2289d8bda99f8a5a61dc8917ea9175236fa48ca8
Miha F <miihafabcic@proton.me>
2025-04-11 09:49:27 +0200

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './Header'
 import Input from './Input'
 
 function App() {
 
     return (
         <>
+            <Header />
             <Input />
             <p>Hello</p>
         </>
@@ -12,7 +14,9 @@ function App() {
 }
 
 // app is my main layout
-// input component
-//  
+// 2 modes: edit, view
+// contact form
+// education form - extends to add more education
+// job experience form - extends
 
 export default App
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <header class="site-header">
+            <h1>My Sticky Header</h1>
+        </header>
+    );
+}
+
+export default Header;
