96ebf0eb6b6bac6d561f98c434139d9cb6920b3b
Miha F <miihafabcic@proton.me>
2025-04-14 10:44:26 +0200

    Add game logic
    - shuffle pokemons on click
    - fetch new pokemons when we guessed too many
    - play again logic

--- src/Game.jsx
+++ src/Game.jsx
@@ -8,6 +8,7 @@ const Game = () => {
     const [currentScore, setCurrentScore] = useState(0);
     const [clickedPokemonsSet, setClickedPokemonsSet] = useState(new Set());
     const [visibleCount, setVisibleCount] = useState(20);
+    const [playing, setPlaying] = useState(true);
     const { data, error, loading } = useGetAllPokemons();
     const [images, setImages] = useState({});
 
@@ -37,15 +38,48 @@ const Game = () => {
     }, [data, visibleCount]);
 
     const handleClick = (name) => {
-        if (clickedPokemonsSet.has(name))
-            console.log("you already clicked pokemon: ", name);
-        else
-            console.log(name);
+        if (clickedPokemonsSet.has(name)) {
+            setPlaying(false);
+            setBestScore(Math.max(bestScore, currentScore));
+            return;
+        }
+
+        const guessed = clickedPokemonsSet.size;
+        const total = Object.keys(images).length;
+        if (guessed / total > 0.5) {
+            console.log("adding pokemons");
+            setVisibleCount(visibleCount + 20);
+        }
+
+        setCurrentScore(currentScore + 1);
         setClickedPokemonsSet((prevSet) => new Set(prevSet).add(name));
     }
 
+    const playAgain = () => {
+        setCurrentScore(0);
+        setPlaying(true);
+    }
+
+    const shuffleAndSliceObject = (obj) => {
+        const entries = Object.entries(obj);
+        for (let i = entries.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [entries[i], entries[j]] = [entries[j], entries[i]];
+        }
+        const shuffled = Object.fromEntries(entries);
+        const first20Keys = Object.keys(shuffled).slice(0, 20);
+        const slicedObject = first20Keys.reduce((obj, key) => {
+            obj[key] = shuffled[key];
+            return obj;
+        }, {});
+        return slicedObject;
+    };
+
+
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error</p>
+
+    const shuffledSlicedImages = shuffleAndSliceObject(images);
 
     return (
         <>
@@ -53,7 +87,13 @@ const Game = () => {
                 <p>Best score: {bestScore}</p>
                 <p>Current score: {currentScore}</p>
             </Header>
-            <ImageGrid images={images} onClick={handleClick} />
+            {playing ?
+                <ImageGrid images={shuffledSlicedImages} onClick={handleClick} />
+                : <div>
+                    <p>More luck next time</p>
+                    <button onClick={playAgain}>Play again</button>
+                </div>
+            }
         </>
     );
 
