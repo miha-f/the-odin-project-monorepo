d7faea8cb949da8ee3e199b575952939d18fb826
Miha F <miihafabcic@proton.me>
2025-04-15 14:14:03 +0200

    [memory_game] Add base value to the vite.config
    - Vite ignores package.json so we need to set vite.config

--- memory_game/vite.config.js
+++ memory_game/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/the-odin-project-monorepo/memory_game/dist/',
   plugins: [react()],
 })
