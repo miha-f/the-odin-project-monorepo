c33b6c47f160bcc92911b2e8f847f7924a68a7f6
Miha F <miihafabcic@proton.me>
2025-04-16 09:08:00 +0200

    [shopping_cart] Add main layout styling

--- shopping_cart/src/layouts/MainLayout.jsx
+++ shopping_cart/src/layouts/MainLayout.jsx
@@ -1,18 +1,38 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const MyNavLink = ({ name, url }) => {
+    return (
+        <NavLink
+            to={url}
+            className={({ isActive }) =>
+                isActive ? 'text-yellow-400' : 'hover:text-yellow-300'
+            }
+        >
+            {name}
+        </NavLink>
+    );
+}
 
 const MainLayout = () => {
     return (
-        <div>
-            <header className="p-4 bg-gray-800 text-white">My App Header
-                <Link to="/">Home</Link> | <Link to="/shop">Shop</Link>
-
+        <div className="min-h-screen flex flex-col">
+            <header className="bg-gray-800 text-white p-4">
+                <nav className="container mx-auto flex items-center justify-between">
+                    <h1 className="text-xl font-semibold">Shopping Cart</h1>
+                    <div className="space-x-4">
+                        <MyNavLink name="Home" url="/" />
+                        <MyNavLink name="Shop" url="/shop" />
+                    </div>
+                </nav>
             </header>
 
-            <main className="p-4">
+            <main className="flex-1 container mx-auto p-4">
                 <Outlet />
             </main>
 
-            <footer className="p-4 bg-gray-200 text-center">© 2025</footer>
+            <footer className="bg-gray-100 text-center text-sm p-4 text-gray-600">
+                © {(new Date().getFullYear())} Shopping cart. All rights reserved.
+            </footer>
         </div>
     );
 }
--- shopping_cart/src/main.jsx
+++ shopping_cart/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import "./index.css";
 
 const router = createBrowserRouter(routes);
 
