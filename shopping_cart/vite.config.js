90dcc56fafe1f682a566180e0edcf459f333e3d1
Miha F <miihafabcic@proton.me>
2025-04-17 09:52:22 +0200

    Revert "[shopping_cart] Make @ symbol work again"
    This reverts commit 78f535889ba1b00052176d93f0961ba07b45f3e6.

--- shopping_cart/vite.config.js
+++ shopping_cart/vite.config.js
@@ -3,20 +3,16 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoBase = '/the-odin-project-monorepo/shopping_cart/dist/';
-
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-    return {
-        base: command === 'build' ? `/${repoBase}/` : '/',
-        plugins: [
-            tailwindcss(),
-            react(),
-        ],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
-            },
+export default defineConfig({
+    base: '/the-odin-project-monorepo/shopping_cart/dist/',
+    plugins: [
+        tailwindcss(),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
         },
-    };
+    },
 })
