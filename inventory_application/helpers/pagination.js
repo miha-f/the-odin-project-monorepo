28cd37a73b4ed042277625c0efe1302fae356ab0
Miha F <miihafabcic@proton.me>
2025-04-23 10:51:56 +0200

    [inventory_application] Add pagination object
    - used for keeping track/managing page

---
+++ inventory_application/helpers/pagination.js
@@ -0,0 +1,38 @@
+const Pagination = (elementsPerPage, totalElements) => {
+    let currentPage = 0;
+
+    const getElementsPerPage = () => elementsPerPage;
+    const getTotalElements = () => totalElements;
+    const getPage = () => currentPage;
+
+    const getTotalPages = () => {
+        const div = Math.floor(totalElements / elementsPerPage);
+        const mod = totalElements % elementsPerPage;
+        if (mod)
+            return div + 1;
+        else
+            return div;
+    };
+
+    const setPage = (newPage) => {
+        // NOTE(miha): Clamp
+        currentPage = Math.max(0, Math.min(newPage, getTotalPages()));
+    };
+
+    const increasePage = () => {
+        // NOTE(miha): Clamp
+        currentPage = Math.max(0, Math.min(currentPage++, getTotalPages()));
+    };
+
+    const decreasePage = () => {
+        // NOTE(miha): Clamp
+        currentPage = Math.max(0, Math.min(currentPage--, getTotalPages()));
+    };
+
+    return {
+        getElementsPerPage, getTotalElements, getPage, getTotalPages,
+        setPage, increasePage, decreasePage
+    };
+}
+
+module.exports = Pagination;
