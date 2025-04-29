76b23ef4a0f687853c817abd2f394fcec5e12f79
Miha F <miihafabcic@proton.me>
2025-04-29 08:55:19 +0200

    [inventory_application] Add tailwind styles to safelist
    - tailwind can't determine which styles we need based on dynamically generated styles.
      If we add them to safelist, tailwind will always have them!

---
+++ inventory_application/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: ["./views/**/*.ejs"],
+    plugins: [],
+    safelist: [
+        'lg:grid-cols-1',
+        'lg:grid-cols-2',
+        'lg:grid-cols-3',
+        'lg:grid-cols-4',
+        'lg:grid-cols-5',
+        'lg:grid-cols-6',
+        'lg:grid-cols-7',
+        'lg:grid-cols-8',
+        'lg:grid-cols-9',
+        'lg:grid-cols-10',
+        'lg:grid-cols-11',
+        'lg:grid-cols-12',
+    ],
+};
