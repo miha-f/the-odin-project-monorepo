4ef8b2008f4577a87518df2a643dbaa5a6c73d6c
Miha F <miihafabcic@proton.me>
2025-04-30 09:24:38 +0200

    Add posts controller skeleton

---
+++ members_only/controllers/postsController.js
@@ -0,0 +1,38 @@
+const postsController = () => {
+    const getAll = () => {
+    };
+
+    const getById = (id) => {
+    };
+
+    const create = () => {
+    };
+
+    const createPost = () => {
+    };
+
+    const removeById = (id) => {
+    };
+
+    const removeByIdPost = (id) => {
+    };
+
+    const updateById = (id) => {
+    };
+
+    const updateByIdPost = (id) => {
+    };
+
+    return {
+        getAll,
+        getById,
+        create,
+        createPost,
+        removeById,
+        removeByIdPost,
+        updateById,
+        updateByIdPost,
+    };
+};
+
+modules.export = postsController;
