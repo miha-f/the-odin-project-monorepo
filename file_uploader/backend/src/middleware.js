53127c9d96232c4ddcea0b41a5ba1bcbf9cf8cbe
Miha F <miihafabcic@proton.me>
2025-05-30 09:04:18 +0200

    Update multer middleware
    - now we save files in the right folder

--- file_uploader/backend/src/middleware.js
+++ file_uploader/backend/src/middleware.js
@@ -1,24 +1,26 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs/promises";
 import { createFolderService } from './services/folder.js';
 
 const FolderService = createFolderService();
 
-// TODO(miha): At the start we should just upload to user root folder.
-// No need to be fancy or anything :)
-
 const multerStorageStrategy = multer.diskStorage({
     destination: async function(req, file, cb) {
         try {
+            // NOTE(miha): We need to put right file under the right folder so
+            // our FS structure is updated.
             const user = req.user;
-            const localPath = path.join(process.env.USERS_STORE_DIR, user.id, "root");
-            cb(null, localPath);
+            const localPath = path.join(user.id, "root", req.params[0]);
+            // TODO(miha): Handle error
+            const { error, result } = await FolderService.fs.create(localPath, true);
+            cb(null, path.join(process.env.USERS_STORE_DIR, localPath));
         } catch (err) {
             cb(err);
         }
     },
     filename: function(req, file, cb) {
+        // NOTE(miha): file.originalname is already renamed name (if user rename in FE). 
+        //   In FE we upload with renamed name!
         cb(null, file.originalname);
     }
 });
