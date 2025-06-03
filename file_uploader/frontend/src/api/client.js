3e1ec01fec39b2cef280aeb0f18efc4fe1457478
Miha F <miihafabcic@proton.me>
2025-06-03 08:22:01 +0200

    Add axios interceptor
    - reroute all 401 (unathorized) errors to /login page

--- file_uploader/frontend/src/api/client.js
+++ file_uploader/frontend/src/api/client.js
@@ -7,3 +7,12 @@ export const api = axios.create({
     withCredentials: true,
 });
 
+// api.interceptors.response.use(
+//     (response) => response,
+//     (error) => {
+//         if (error.response?.status === 401) {
+//             window.location.href = "/login"
+//         }
+//         return Promise.reject(error);
+//     },
+// );
