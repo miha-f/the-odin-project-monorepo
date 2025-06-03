81c70ed170d69cf09116171f881b12a34e183b60
Miha F <miihafabcic@proton.me>
2025-06-03 08:22:33 +0200

    Update search feature

--- file_uploader/frontend/src/features/search/components/FolderEntry.jsx
+++ file_uploader/frontend/src/features/search/components/FolderEntry.jsx
@@ -4,7 +4,7 @@ import { faFolder } from '@fortawesome/free-solid-svg-icons';
 
 const FileEntry = ({ file }) => {
     return (
-        <div key={file.id}>
+        <div>
             <ul className="ml-2">
                 <div className="bg-surface rounded shadow-sm overflow-hidden border-b">
                     <div className="grid grid-cols-1 items-center px-4 py-2 cursor-pointer hover:bg-white truncate">
@@ -58,7 +58,7 @@ export const FolderEntry = ({ folderPath, files }) => {
             {/* NOTE(miha): Files entry */}
             <ul className="ml-2">
                 {files.map((file) => (
-                    <FileEntry file={file} />
+                    <FileEntry key={file.id} file={file} />
                 ))}
             </ul>
         </div>
