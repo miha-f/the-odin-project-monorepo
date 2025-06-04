dd63263d52b9597abea0afcbb963a5fc5f3c1569
Miha F <miihafabcic@proton.me>
2025-06-04 20:57:48 +0200

    Add blog model

---
+++ blog/backend/src/models/blog.model.ts
@@ -0,0 +1,22 @@
+type Uuid = string;
+
+export interface Blog {
+    id: number;
+    authorId: Uuid;
+    title: string;
+    content: string;
+    image?: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export const isBlog = (obj: any): obj is Blog => {
+    return (
+        typeof obj.id === 'number' &&
+        typeof obj.authorId === 'string' &&
+        typeof obj.title === 'string' &&
+        typeof obj.content === 'string' &&
+        obj.createdAt instanceof Date &&
+        obj.updatedAt instanceof Date
+    );
+}
