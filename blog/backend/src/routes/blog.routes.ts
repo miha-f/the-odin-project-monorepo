ba53045873e8c22a4572e5c4c89bfd64d61cb13c
Miha F <miihafabcic@proton.me>
2025-06-04 20:59:53 +0200

    Add all blog CRUD endpoints

--- blog/backend/src/routes/blog.routes.ts
+++ blog/backend/src/routes/blog.routes.ts
@@ -1,9 +1,31 @@
 import express from "express";
-import { getDummyBlog } from "@/services/blog.service";
 
 const router = express.Router();
 
-router.get("/dummy", async (_req, res) => {
-    const blog = await getDummyBlog();
-    res.status(200).json(blog);
+router.get("/", async (_req, res) => {
+    res.status(200).json({ msg: "ok" });
 });
+
+router.post("/", async (_req, res) => {
+    res.status(200).json({ msg: "ok" });
+});
+
+router.get("/:blogId", async (_req, res) => {
+    res.status(200).json({ msg: "ok" });
+});
+
+router.patch("/:blogId", async (_req, res) => {
+    res.status(200).json({ msg: "ok" });
+});
+
+router.delete("/:blogId", async (_req, res) => {
+    res.status(200).json({ msg: "ok" });
+});
+
+// POST /blogs -> create new blog
+// GET  /blogs -> get all blogs
+// GET  /blogs/:id -> get specific blog
+// PATCH /blogs/:id -> update specific blog (if we are the blog owner)
+// DELETE /blogs/:id -> delete specific user (if we are the blog owner)
+
+export default router;
