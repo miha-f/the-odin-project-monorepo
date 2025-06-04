4a28778466efdf6383b6d236e497acee4af99323
Miha F <miihafabcic@proton.me>
2025-06-04 22:21:47 +0200

    Add blog routes

--- blog/backend/src/routes/blog.routes.ts
+++ blog/backend/src/routes/blog.routes.ts
@@ -1,31 +1,63 @@
 import express from "express";
+import { createBlogService } from "@/services/blog.service";
+import mockDb from "@/db/mockDb";
+import { handleAppError } from "@/utils/handleAppError";
 
 const router = express.Router();
 
+const blogService = createBlogService({ db: mockDb });
+
+// NOTE(miha): Get all blogs.
 router.get("/", async (_req, res) => {
-    res.status(200).json({ msg: "ok" });
+    let [blog, err] = await blogService.getAll();
+    if (err) {
+        const { status, body } = handleAppError(err);
+        res.status(status).json(body);
+    }
+    res.status(200).json({ data: blog });
 });
 
-router.post("/", async (_req, res) => {
-    res.status(200).json({ msg: "ok" });
+// NOTE(miha): Get specific blog with blogId (number).
+router.get("/:blogId", async (req, res) => {
+    const blogId = Number(req.params.blogId);
+    let [blog, err] = await blogService.getById(blogId);
+    if (err) {
+        const { status, body } = handleAppError(err);
+        res.status(status).json(body);
+    }
+    res.status(200).json({ data: blog });
 });
 
-router.get("/:blogId", async (_req, res) => {
-    res.status(200).json({ msg: "ok" });
+// NOTE(miha): Create new blog, {title: "", content: ""} are required.
+router.post("/", async (req, res) => {
+    let [blog, err] = await blogService.create(req.body);
+    if (err) {
+        const { status, body } = handleAppError(err);
+        res.status(status).json(body);
+    }
+    res.status(200).json({ data: blog });
 });
 
-router.patch("/:blogId", async (_req, res) => {
-    res.status(200).json({ msg: "ok" });
+// NOTE(miha): Update blog with blogId (number), can pass empty body - no update.
+router.patch("/:blogId", async (req, res) => {
+    const blogId = Number(req.params.blogId);
+    let [blog, err] = await blogService.update(blogId, req.body);
+    if (err) {
+        const { status, body } = handleAppError(err);
+        res.status(status).json(body);
+    }
+    res.status(200).json({ data: blog });
 });
 
-router.delete("/:blogId", async (_req, res) => {
-    res.status(200).json({ msg: "ok" });
+// NOTE(miha): Remove blog with blogId (number).
+router.delete("/:blogId", async (req, res) => {
+    const blogId = Number(req.params.blogId);
+    let [blog, err] = await blogService.remove(blogId);
+    if (err) {
+        const { status, body } = handleAppError(err);
+        res.status(status).json(body);
+    }
+    res.status(200).json({ data: blog });
 });
-
-// POST /blogs -> create new blog
-// GET  /blogs -> get all blogs
-// GET  /blogs/:id -> get specific blog
-// PATCH /blogs/:id -> update specific blog (if we are the blog owner)
-// DELETE /blogs/:id -> delete specific user (if we are the blog owner)
 
 export default router;
