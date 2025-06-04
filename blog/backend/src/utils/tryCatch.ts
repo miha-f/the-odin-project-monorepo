ac470084206051ea16acfdf63d3324d57e5b7e92
Miha F <miihafabcic@proton.me>
2025-06-04 20:58:34 +0200

    Update tryCatch

--- blog/backend/src/utils/tryCatch.ts
+++ blog/backend/src/utils/tryCatch.ts
@@ -1,12 +1,12 @@
-import { internalError, type AppError } from "@/errors/errors";
-
 export async function tryCatch<T>(
     fn: () => Promise<T>
-): Promise<[T | null, AppError | null]> {
+): Promise<[T | null, Error | null]> {
     try {
         const data = await fn();
         return [data, null];
     } catch (e) {
-        return [null, internalError("Unexpected error", e)];
+        if (e instanceof Error)
+            return [null, e];
+        return [null, new Error(String(e))];
     }
 }
