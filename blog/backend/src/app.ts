938cf7f5529a8d9904fcd667d3fe3917ef66f71d
Miha F <miihafabcic@proton.me>
2025-06-04 22:22:36 +0200

    Add pino-http middleware
    - logs every http request

--- blog/backend/src/app.ts
+++ blog/backend/src/app.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
+import pinoHttp from 'pino-http';
 import router from "@/routes";
+import { logger } from "@/utils/logger";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/hello", (_req, res) => {
-    res.send("Hello, world!");
-});
+app.use(pinoHttp({ logger }));
 
 app.use(router);
 
