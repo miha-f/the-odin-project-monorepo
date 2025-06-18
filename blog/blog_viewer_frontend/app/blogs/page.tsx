0346376af0d93017f13db07ea9ecb39e18cbe556
Miha F <miihafabcic@proton.me>
2025-06-18 14:14:45 +0200

    Add blogs and users tables
    - show all info (blogs, users) in a table

--- blog/blog_viewer_frontend/app/blogs/page.tsx
+++ blog/blog_viewer_frontend/app/blogs/page.tsx
@@ -1,6 +1,55 @@
+import { Center, Text, Table, TableTr, TableTh, TableTd, TableThead, TableTbody, TableScrollContainer } from '@mantine/core';
+import { api } from "@/lib/axiosClient";
+import { tryCatch } from "@/utils/tryCatch";
+import { Blog } from "@/utils/models";
+import { formatDistance } from "date-fns";
+import Link from "next/link";
 
-export default async function BlogsPage() {
+export default async function Page() {
+    const { data: blogs, error: blogsErr } = await tryCatch<Blog[]>(api.get(`/blogs`));
+
+    if (blogsErr) {
+        return (
+            <Center>
+                <Text c="red" size="lg" mt="xl">
+                    Failed to load.
+                </Text>
+            </Center>
+        );
+    }
+
+    if (!blogs) {
+        return (
+            <Center>
+                <Text size="lg" mt="xl">
+                    Not found.
+                </Text>
+            </Center>
+        );
+    }
+
+    const rows = blogs.map((blog) => (
+        <TableTr key={blog.id}>
+            <TableTd><Link href={`/blogs/${blog.id}`}>{blog.id}</Link></TableTd>
+            <TableTd><Link href={`/blogs/${blog.id}`}>{blog.title}</Link></TableTd>
+            <TableTd><Link href={`/users/${blog.authorId}`}>{blog.authorId}</Link></TableTd>
+            <TableTd>{formatDistance(blog.updatedAt, new Date(), { addSuffix: true })}</TableTd>
+        </TableTr>
+    ));
+
     return (
-        <>table of all blogs</>
+        <TableScrollContainer minWidth={500}>
+            <Table>
+                <TableThead>
+                    <TableTr>
+                        <TableTh>Id</TableTh>
+                        <TableTh>Blog Title</TableTh>
+                        <TableTh>Author</TableTh>
+                        <TableTh>Created</TableTh>
+                    </TableTr>
+                </TableThead>
+                <TableTbody>{rows}</TableTbody>
+            </Table>
+        </TableScrollContainer>
     );
 }
--- blog/blog_viewer_frontend/app/users/page.tsx
+++ blog/blog_viewer_frontend/app/users/page.tsx
@@ -1,6 +1,51 @@
+import { Center, Text, Table, TableTr, TableTh, TableTd, TableThead, TableTbody, TableScrollContainer } from '@mantine/core';
+import { api } from "@/lib/axiosClient";
+import { tryCatch } from "@/utils/tryCatch";
+import { User } from "@/utils/models";
+import { formatDistance } from "date-fns";
+import Link from "next/link";
 
-export default async function UsersPage() {
+export default async function Page() {
+    const { data: users, error: usersErr } = await tryCatch<User[]>(api.get(`/users`));
+
+    if (usersErr) {
+        return (
+            <Center>
+                <Text c="red" size="lg" mt="xl">
+                    Failed to load.
+                </Text>
+            </Center>
+        );
+    }
+
+    if (!users) {
+        return (
+            <Center>
+                <Text size="lg" mt="xl">
+                    Not found.
+                </Text>
+            </Center>
+        );
+    }
+
+    const rows = users.map((user) => (
+        <TableTr key={user.uuid}>
+            <TableTd><Link href={`/users/${user.uuid}`}>{user.username}</Link></TableTd>
+            <TableTd>{formatDistance(user.updatedAt, new Date(), { addSuffix: true })}</TableTd>
+        </TableTr>
+    ));
+
     return (
-        <>table of all users</>
+        <TableScrollContainer minWidth={500}>
+            <Table>
+                <TableThead>
+                    <TableTr>
+                        <TableTh>Username</TableTh>
+                        <TableTh>Created</TableTh>
+                    </TableTr>
+                </TableThead>
+                <TableTbody>{rows}</TableTbody>
+            </Table>
+        </TableScrollContainer>
     );
 }
